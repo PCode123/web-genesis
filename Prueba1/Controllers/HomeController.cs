f47b0a54e46be633a47c8578f45540558869d7d6
PCode123 <yllescaspiero@gmail.com>
2023-09-30 15:48:28 -0500

    first commit

---
+++ Prueba1/Controllers/HomeController.cs
@@ -0,0 +1,198 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Prueba1.Models;
+using System.Diagnostics;
+using System.Net.Mail;
+using System.Net;
+
+namespace Prueba1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration )
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Formulario()
+        {
+            return View();
+        }
+
+        public IActionResult about()
+        {
+            return View();
+        }
+
+        public IActionResult courses()
+        {
+            return View();
+        }
+
+        public IActionResult Exito()
+        {
+            return View();
+        }
+
+        /* Areas */
+
+        public IActionResult AreaAgil()
+        {
+            return View();
+        }
+
+        public IActionResult AreaData()
+        {
+            return View();
+        }
+        public IActionResult AreaCiber()
+        {
+            return View();
+        }
+        public IActionResult AreaGestion()
+        {
+            return View();
+        }
+
+        /* Fin Areas */
+
+        /*Cursos*/
+
+        public IActionResult CUBPM()
+        {
+            return View();
+        }
+
+        public IActionResult CUBusinessSQL()
+        {
+            return View();
+        }
+
+        public IActionResult CUCiberSecurity()
+        {
+            return View();
+        }
+        public IActionResult CUDashboardPBI()
+        {
+            return View();
+        }
+        public IActionResult CUDataScience()
+        {
+            return View();
+        }
+        public IActionResult CUDatosR()
+        {
+            return View();
+        }
+        public IActionResult CUISO56002()
+        {
+            return View();
+        }
+        public IActionResult CUOKR()
+        {
+            return View();
+        }
+        public IActionResult CUPython()
+        {
+            return View();
+        }
+        public IActionResult CUScrum()
+        {
+            return View();
+        }
+        public IActionResult CUStartup()
+        {
+            return View();
+        }
+
+        /* Fin Cursos */
+
+        [HttpPost]
+        public ActionResult EnviarCorreo(IFormCollection form)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(form["apellidoPaterno"]) || string.IsNullOrEmpty(form["apellidoMaterno"]) || string.IsNullOrEmpty(form["nombres"]) || string.IsNullOrEmpty(form["tipoDocumento"]) || string.IsNullOrEmpty(form["numeroDocumento"]) || string.IsNullOrEmpty(form["celular"]) || string.IsNullOrEmpty(form["correoElectronico"]) || string.IsNullOrEmpty(form["correoElectronico"]) || string.IsNullOrEmpty(form["tipodeCurso"]) && !bool.Parse(form["consentimiento"]))
+                {
+                    // Si la validación falla, puedes redirigir al usuario a una página de error o volver a mostrar el formulario con mensajes de error.
+                    ModelState.AddModelError(string.Empty, "Por favor, complete todos los campos y acepte el consentimiento.");
+                    return View("Index"); // Reemplaza "NombreDeTuVista" con el nombre de tu vista
+                }
+
+                // Recuperar los datos del formulario
+                string apellidoPaterno = form["apellidoPaterno"];
+                string apellidoMaterno = form["apellidoMaterno"];
+                string nombres = form["nombres"];
+                string tipoDocumento = form["tipoDocumento"];
+                string numeroDocumento = form["numeroDocumento"];
+                string celular = form["celular"];
+                string correoElectronico = form["correoElectronico"];
+                string Curso = form["tipodeCurso"];
+
+                // Configurar el mensaje de correo
+                var mensaje = new MailMessage
+                {
+                    From = new MailAddress(_configuration["EmailSettings:Sender"]),
+                    Subject = "Solicitud de información",
+                    Body = $"Apellido Paterno: {apellidoPaterno}\n" +
+                           $"Apellido Materno: {apellidoMaterno}\n" +
+                           $"Nombres: {nombres}\n" +
+                           $"Tipo de Documento: {tipoDocumento}\n" +
+                           $"Número de Documento: {numeroDocumento}\n" +
+                           $"Celular: {celular}\n" +
+                           $"Correo Electrónico: {correoElectronico}\n" +
+                           $"Curso: {Curso}"
+
+                };
+
+                mensaje.To.Add("ronaldo@genesiscenterit.com");
+
+                // Configurar el cliente SMTP
+                var smtpCliente = new SmtpClient
+                {
+                    Host = _configuration["EmailSettings:MailServer"], // o el host de tu servidor SMTP
+                    Port = _configuration.GetValue<int>("EmailSettings:MailPort"),
+                    Credentials = new NetworkCredential(_configuration["EmailSettings:Sender"], _configuration["EmailSettings:Password"]),
+                    EnableSsl = true
+                };
+
+                // Enviar el correo electrónico
+                smtpCliente.Send(mensaje);
+
+                // Redireccionar a una página de éxito
+                return RedirectToAction("Exito");
+            }
+            catch (Exception ex)
+            {
+                // Manejar cualquier error y redireccionar a una página de error
+                ViewBag.ErrorMessage = ex.Message;
+                return View("Error");
+            }
+        }
+
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Prueba1/wwwroot/js/main.js
@@ -0,0 +1,132 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').css('top', '0px');
+        } else {
+            $('.sticky-top').css('top', '-100px');
+        }
+    });
+    
+    
+    // Dropdown on mouse hover
+    const $dropdown = $(".dropdown");
+    const $dropdownToggle = $(".dropdown-toggle");
+    const $dropdownMenu = $(".dropdown-menu");
+    const showClass = "show";
+    
+    $(window).on("load resize", function() {
+        if (this.matchMedia("(min-width: 992px)").matches) {
+            $dropdown.hover(
+            function() {
+                const $this = $(this);
+                $this.addClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "true");
+                $this.find($dropdownMenu).addClass(showClass);
+            },
+            function() {
+                const $this = $(this);
+                $this.removeClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "false");
+                $this.find($dropdownMenu).removeClass(showClass);
+            }
+            );
+        } else {
+            $dropdown.off("mouseenter mouseleave");
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 2000,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+    $(document).ready(function () {
+        $(".header-carousel").owlCarousel({
+            items: 3, // Mostrar 3 elementos a la vez
+            loop: true, // Repetir el carrusel
+            margin: 20, // Espacio entre elementos
+            nav: true, // Mostrar botones de navegación
+            responsive: {
+                0: {
+                    items: 1 // En pantallas pequeñas, mostrar 1 elemento a la vez
+                },
+                768: {
+                    items: 2 // En pantallas medianas, mostrar 2 elementos a la vez
+                },
+                992: {
+                    items: 3 // En pantallas grandes, mostrar 3 elementos a la vez
+                }
+            }
+        });
+    });
+
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 24,
+        dots: true,
+        loop: true,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+
+    
+})(jQuery);
+
